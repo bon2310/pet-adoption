2a554a18ac1b182a264fd81a49aa7674985f9c5f
Brian <boneill2310@gmail.com>
2024-02-07 01:41:24 +0000

    Updating files

---
+++ main.js
@@ -0,0 +1,16 @@
+async function start() {
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await weatherPromise.json()
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector("#temperature-output").textContent = ourTemperature
+
+  console.log(ourTemperature)
+}
+
+start()
+
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  console.log(petsData)
+}
