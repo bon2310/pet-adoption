c0f069afba4cb0579e773c9ded87261162f5b015
Brian <boneill2310@gmail.com>
2024-02-07 19:41:26 +0000

    Finished pet filtering

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const template = document.querySelector("#pet-card-template")
 const wrapper = document.createDocumentFragment()
 
-
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
   const weatherData = await weatherPromise.json()
+
   const ourTemperature = weatherData.properties.periods[0].temperature
   document.querySelector("#temperature-output").textContent = ourTemperature
 
-  console.log(ourTemperature)
 }
 
 start()
@@ -16,20 +15,22 @@ start()
 async function petsArea() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
   const petsData = await petsPromise.json()
-  console.log(petsData)
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
 
+    clone.querySelector(".pet-card").dataset.species = pet.species
+
     clone.querySelector("h3").textContent = pet.name
-    clone.querySelector("p.pet-description").textContent = pet.description
-    clone.querySelector("p.pet-age").textContent = createAgeText(pet.birthYear)
+    clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+
     if (!pet.photo) pet.photo = "images/fallback.jpg"
-    clone.querySelector("img").src = pet.photo
+
+    clone.querySelector(".pet-card-photo img").src = pet.photo
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
 
     wrapper.appendChild(clone)
-    console.log(pet.photo)
   })
-
   document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 
@@ -39,12 +40,13 @@ function createAgeText(birthYear) {
   const currentYear = new Date().getFullYear()
   const age = currentYear - birthYear
 
-  if (age == 1) return "1 Year old"
-  if (age == 0) return "Less than a Year Old"
-  return `${age} Years Old`
+  if (age == 1) return "1 year old"
+  if (age == 0) return "Less than a year old"
+
+  return `${age} years old`
 }
 
-//Pet filter button code
+// pet filter button code
 const allButtons = document.querySelectorAll(".pet-filter button")
 
 allButtons.forEach(el => {
@@ -59,4 +61,12 @@ function handleButtonClick(e) {
   e.target.classList.add("active")
 
   // actually filter the pets down below
+  const currentFilter = e.target.dataset.filter
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid"
+    } else {
+      el.style.display = "none"
+    }
+  })
 }
